e545c7ad3de066dc53879d89ba847b2cd6964099
Yuna-commits <ksm13563661@gmail.com>
2025-11-14 09:24:51 +0900

    refactor(auth): App.css 삭제, #root 설정을 AuthLayout에 추가

--- frontend/src/components/auth/AuthLayout.jsx
+++ frontend/src/components/auth/AuthLayout.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 
 export default function AuthLayout({ children }) {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-neutral-100">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-neutral-100 p-8 text-center sm:p-10 lg:p-12">
       <div className="mx-auto w-full max-w-md rounded-xl border border-gray-300 bg-white p-8 shadow-sm">
         {children}
       </div>
